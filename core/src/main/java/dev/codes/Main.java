35a65e5851ae7a18c1679e62b86edc64327ad25e
Yip Ka Yan <yipkayan3@gmail.com>
2022-07-06 00:16:57 +0800

    updated logging to include spring framework logging.

--- core/src/main/java/dev/codes/Main.java
+++ core/src/main/java/dev/codes/Main.java
@@ -2,13 +2,29 @@ package dev.codes;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
+    private static final String CONFIG_LOCATION = "beans.xml";
     public static void main(String[] args)
     {
         log.info("Guess the number game");
         log.info("logging module successfully imported!!");
+
+        //1. create context (container)
+        //ApplicationContext context = new ClassPathXmlApplicationContext("services.xml", "daos.xml");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean("numberGenerator",NumberGenerator.class);
+
+        int number = numberGenerator.next();
+
+        log.info("number = {}",number);
+
+        //to prevent memory loss
+        context.close();
     }
 }
---
+++ core/src/main/java/dev/codes/NumberGenerator.java
@@ -0,0 +1,7 @@
+package dev.codes;
+
+public interface NumberGenerator {
+
+    int next();
+    int getMaxNumber();
+}
