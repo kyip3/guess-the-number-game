009817bf470ec17b267bc199cd4b292971a8be65
Yip Ka Yan <yipkayan3@gmail.com>
2022-07-06 21:44:43 +0800

    added properties file.
    added default value if properties file cannot be found

--- core/src/main/java/dev/codes/console/config/GameConfig.java
+++ core/src/main/java/dev/codes/console/config/GameConfig.java
@@ -2,16 +2,26 @@ package dev.codes.console.config;
 
 import dev.codes.console.GuessCount;
 import dev.codes.console.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
+//resources folder is at the classpath.
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
     // == fields ==
-    private int maxNumber = 25;
 
-    private int guessCount = 8;
+    //default value is 20 if cannot find properties file
+    @Value("${game.maxNumber:20}")
+    private int maxNumber;
+
+    //default value is 5 if cannot find properties file
+    @Value("${game.guessCount:5}")
+    private int guessCount;
 
     // == bean methods ==
     @Bean
