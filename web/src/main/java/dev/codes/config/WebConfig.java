c6ac6a5225aefd02b88d5de0d365ac4fdff1abd4
Yip Ka Yan <yipkayan3@gmail.com>
2022-07-16 23:35:49 +0800

    added thymeleaf configuration.

---
+++ web/src/main/java/dev/codes/config/WebConfig.java
@@ -0,0 +1,19 @@
+package dev.codes.config;
+
+import dev.codes.util.ViewNames;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+
+        //setting / path to home.html
+        //http://localhost:8080/
+        registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+}
---
+++ web/src/main/java/dev/codes/util/ViewNames.java
@@ -0,0 +1,9 @@
+package dev.codes.util;
+
+public final class ViewNames {
+    // == constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames(){} // add a private constructor so it cant be instantiated.
+}
